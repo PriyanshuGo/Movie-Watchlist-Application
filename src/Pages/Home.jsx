f2859c17ecee8e4053efd0246124c32013e86e87
PriyanshuGo <priyanshusinghrawat84@gmail.com>
2025-03-26 13:24:13 +0530

    whislist page responsive

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,9 +7,7 @@ function Home() {
     <div className="flex flex-col items-center h-full">
       <Head />
       <Joke />
-      <div className=" bg-gray-800/50 backdrop-blur-sm rounded-xl p-6 shadow-lg border border-gray-700 w-full max-w-4xl mt-6">
-        <Whislist />
-      </div>
+      <Whislist />
     </div>
   );
 }

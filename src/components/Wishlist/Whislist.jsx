478fed5cc7e1446f78a60318ef2d6d747009ffd6
PriyanshuGo <priyanshusinghrawat84@gmail.com>
2025-03-27 16:01:07 +0530

    redirect to movie detail page from whislist page

--- src/components/Wishlist/Whislist.jsx
+++ src/components/Wishlist/Whislist.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { MovieContext } from "../../contextCreate/Movie";
+import { Link } from "react-router-dom";
 
 function Whislist() {
   const { watchLater, setWatchLater } = useContext(MovieContext);
@@ -18,32 +19,34 @@ function Whislist() {
         <p className="text-gray-400">No movies added to wishlist.</p>
       ) : (
         <div className="flex flex-wrap justify-center gap-6">
-          {watchLater.map((movie) => (
-            <div
-              key={movie.id}
-              className="bg-gray-800 p-4 rounded-lg shadow-lg w-48 sm:w-56 md:w-64 flex flex-col items-center"
-            >
-              {/* Movie Poster */}
-              <img
-                src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                alt={movie.title}
-                className="w-full h-60 object-cover rounded-lg"
-              />
-
-              {/* Movie Title */}
-              <h3 className="text-lg font-semibold text-white mt-2 text-center">
-                {movie.title}
-              </h3>
-
-              {/* Remove Button */}
-              <button
-                onClick={() => handleRemove(movie.id)}
-                className="mt-2 bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-md text-sm transition duration-300"
+            {watchLater.map((movie) => (
+              <div
+                key={movie.id}
+                className="bg-gray-800 p-4 rounded-lg shadow-lg w-48 sm:w-56 md:w-64 flex flex-col items-center"
               >
-                Remove
-              </button>
-            </div>
-          ))}
+          <Link to={`/movie/${movie.id}`}>
+                {/* Movie Poster */}
+                <img
+                  src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                  alt={movie.title}
+                  className="w-full h-60 object-cover rounded-lg"
+                  />
+                  </Link>
+
+                {/* Movie Title */}
+                <h3 className="text-lg font-semibold text-white mt-2 text-center">
+                  {movie.title}
+                </h3>
+
+                {/* Remove Button */}
+                <button
+                  onClick={() => handleRemove(movie.id)}
+                  className="mt-2 bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-md text-sm transition duration-300"
+                >
+                  Remove
+                </button>
+              </div>
+            ))}
         </div>
       )}
     </div>
